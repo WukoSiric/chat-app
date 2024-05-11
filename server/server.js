ef3c2ca2515573219d9c0758325b8689803e9cf1
WukoSiric <lstainer2002@gmail.com>
2024-05-11 21:58:39 +0930

    Add Socket.IO for real-time communication

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const http = require('http'); 
+require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 const app = express();
-require('dotenv').config();
+const server = http.createServer(app); // Create an HTTP server
+const io = new Server(server); // Attach Socket.IO to the HTTP server
+
 
 // Middleware
 app.use(cors());
@@ -12,10 +17,10 @@ app.use(express.json());
 // Connect to MongoDB
 console.log(`Connecting to database at ${process.env.MONGO_URI}`.substring(0, 50) + '...');
 mongoose.connect(process.env.MONGO_URI, {})
-    .then( () => { 
-        console.log('Connected to database ');
+    .then(() => { 
+        console.log('Connected to database');
     })
-    .catch( (err) => { 
+    .catch((err) => { 
         console.error(`Error connecting to the database. \n${err}`);
     });
 
@@ -26,6 +31,11 @@ app.get('/api', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+// Socket.IO logic goes here
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
