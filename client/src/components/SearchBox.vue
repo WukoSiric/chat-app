21677c560cb5a65c5a59ea6d679663fbcfca5378
WukoSiric <lstainer2002@gmail.com>
2024-05-17 20:56:55 +0930

    Refactor SearchBox component

--- client/src/components/SearchBox.vue
+++ client/src/components/SearchBox.vue
@@ -2,74 +2,75 @@
 import MagnifyIcon from '../assets/svg/MagnifyIcon.vue';
 
 export default {
-    components: {
-        MagnifyIcon
-    },
-    props: {
-        query: {
-            type: String,
-            required: true
-        },
-        placeholder: { 
-            type: String,
-            default: 'Enter text here',
-            required: false
-        }
-    },
-    data() {
-        return {
-            internvalValue: this.modelValue
-        }
-    },
-    watch: {
-            modelValue(newValue) {
-                this.internalValue = newValue;
-            },
-    },
-    methods: {
-        updateValue(event) {
-            this.internalValue = event.target.value;
-            this.$emit('update:modelValue', this.internalValue);
-        }
-    }
+	components: {
+		MagnifyIcon
+	},
+	props: {
+		query: {
+			type: String,
+			required: true
+		},
+		placeholder: {
+			type: String,
+			default: 'Enter text here',
+			required: false
+		}		
+	},
+	data() {
+		return {
+			internvalValue: this.modelValue
+		}
+	},
+	watch: {
+		modelValue(newValue) {
+			this.internalValue = newValue;
+		},
+	},
+	methods: {
+		updateValue(event) {
+			this.internalValue = event.target.value;
+			this.$emit('update:modelValue', this.internalValue);
+		}
+	}
 };
 </script>
 
 <template>
-    <div id="search-box-container">
-        <!-- <MagnifyIcon id="search-icon"/> -->
-        <input id="search-box" type="text" autocomplete="off" :value="internalValue" @input="updateValue" :placeholder="placeholder"/>
-    </div>
+	<div id="search-box-container">
+		<!-- <MagnifyIcon id="search-icon"/> -->
+		<input id="search-box" type="text" autocomplete="off" :value="internalValue" @input="updateValue"
+			:placeholder="placeholder" />
+	</div>
 </template>
 
 <style>
-    ::placeholder {
-        color: rgb(190, 190, 190); 
-        font-weight: 500;
-    }
+::placeholder {
+	color: rgb(190, 190, 190);
+	font-weight: 500;
+}
 
-    #search-box { 
-        margin: 20px;
-        padding: 10px;
-        width: 90%;
-        border: 2px solid white;
-        color: white;
-        border-radius: 10px;
-        font-size: 15px;
-        background-color: transparent;
-        
-    }
+#search-box {
+	margin: 20px;
+	padding: 10px;
+	width: 90%;
+	border: 2px solid white;
+	color: white;
+	border-radius: 10px;
+	font-size: 15px;
+	background-color: transparent;
 
-    #search-box:focus {
-        outline: 1px solid rgb(88, 88, 88);
-    }
+}
 
-    #search-box-container { 
-        display: flex;
-    }
+#search-box:focus {
+	outline: 1px solid rgb(88, 88, 88);
+}
 
-    #search-icon { 
-        fill: white;
-        max-height: 40px;
-    }
+#search-box-container {
+	display: flex;
+}
+
+#search-icon {
+	fill: white;
+	max-height: 40px;
+}
 </style>
