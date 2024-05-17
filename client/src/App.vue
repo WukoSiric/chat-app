5b21ea85a7ae7cfe5068aea26cf9a93eed0ebb45
WukoSiric <lstainer2002@gmail.com>
2024-05-17 20:57:02 +0930

    Add MeetButton component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,140 +1,146 @@
 <script>
-import io from 'socket.io-client'; 
+import io from 'socket.io-client';
 import InputBox from './components/InputBox.vue';
 import MessageBox from './components/MessageBox.vue';
 import SearchBox from './components/SearchBox.vue';
+import MeetButton from './components/MeetButton.vue';
 
 export default {
-  components: {
-    InputBox,
-    MessageBox,
-    SearchBox
-  },
-  data() {
-    return {
-      inputValue: '', // Initialize input value as an empty string
-      databases: [],
-      messages: [], 
-      username: '', 
-      socket: null,
-      query: ''
-    };
-  },
-  methods: {
-    async generateUsername() {
-      try { 
-        const response = await fetch('http://localhost:3000' + '/generateUsername');
-        if (response.ok) {
-          const data = await response.json(); 
-          this.username = data.username;
-        } 
-        else {
-          // Response was not okay
-        }
-      } catch (error) {
-        console.error('Error reaching server: ', error); 
-      }
-    },
-    sendMessage() {
-      if (this.inputValue === '') {
-        return;
-      }
-      this.socket.emit('message', {username: this.username, message: this.inputValue});
-      this.inputValue = '';
-    },
-  },
-  mounted() {
-    this.generateUsername();
+	components: {
+		InputBox,
+		MessageBox,
+		SearchBox,
+		MeetButton
+	},
+	data() {
+		return {
+			inputValue: '', // Initialize input value as an empty string
+			databases: [],
+			messages: [],
+			username: '',
+			socket: null,
+			query: ''
+		};
+	},
+	methods: {
+		async generateUsername() {
+			try {
+				const response = await fetch('http://localhost:3000' + '/generateUsername');
+				if (response.ok) {
+					const data = await response.json();
+					this.username = data.username;
+				}
+				else {
+					// Response was not okay
+				}
+			} catch (error) {
+				console.error('Error reaching server: ', error);
+			}
+		},
+		sendMessage() {
+			if (this.inputValue === '') {
+				return;
+			}
+			this.socket.emit('message', { username: this.username, message: this.inputValue });
+			this.inputValue = '';
+		},
+	},
+	mounted() {
+		this.generateUsername();
 
-    this.socket = io('http://localhost:3000');
+		this.socket = io('http://localhost:3000');
 
-    this.socket.on('connect', () => {
-      console.log('Connected to socket.io server!');
-    });
+		this.socket.on('connect', () => {
+			console.log('Connected to socket.io server!');
+		});
 
-    this.socket.on('disconnect', () => {
-      console.log('Disconnected from socket.io server!');
-    });
+		this.socket.on('disconnect', () => {
+			console.log('Disconnected from socket.io server!');
+		});
 
-    this.socket.on('message', ({username, message}) => {
-      console.log('Received message:', message);
-      const isReceived = username !== this.username;
-      this.messages.push({username, message, isReceived});
-    });
+		this.socket.on('message', ({ username, message }) => {
+			console.log('Received message:', message);
+			const isReceived = username !== this.username;
+			this.messages.push({ username, message, isReceived });
+		});
 
-    this.username = 'Anonymous';
-  },
-  computed: {
-    usersMatch (messageObject) {
-      return messageObject.username === this.username;
-    }
-  }
+		this.username = 'Anonymous';
+	},
+	computed: {
+		usersMatch(messageObject) {
+			return messageObject.username === this.username;
+		}
+	}
 }; 
 </script>
 
 <template>
-  <div>
-    <div class='app'>
-      <div class='menu'>
-        {{ username }}
-        <SearchBox :modelValue='query' placeholder='Search'/>
-      </div>
-      <div class='chat-window'>
-        <div class="messages">
-          <MessageBox v-for='messageObject in messages' :key='messageObject' :messageObject='messageObject' :isReceived='messageObject.isReceived'></MessageBox>
-        </div>
-          <form @submit.prevent='sendMessage'>
-            <InputBox id='chat-input' v-model='inputValue' placeholder='Type a chat message...'/>
-          </form>
-      </div>
-    </div>
-  </div>
-  <div>
+	<div>
+		<div class='app'>
+			<div class='menu'>
+				{{ username }}
+				<MeetButton />
+				<SearchBox :modelValue='query' placeholder='Search' />
+			</div>
+			<div class='chat-window'>
+				<div class="messages">
+					<MessageBox v-for='messageObject in messages' :key='messageObject' :messageObject='messageObject'
+						:isReceived='messageObject.isReceived'></MessageBox>
+				</div>
+				<form @submit.prevent='sendMessage'>
+					<InputBox id='chat-input' v-model='inputValue' placeholder='Type a chat message...' />
+				</form>
+			</div>
+		</div>
+	</div>
+	<div>
 
-  </div>
+	</div>
 </template>
 
 <style>
-  html {
-    font-family: 'sarabun', sans-serif;
-  }
+html {
+	font-family: 'sarabun', sans-serif;
+}
 
-  .messages {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    margin: 20px 20px 0px 20px;
-    height: 90%;
-    overflow-y : auto;
-  }
+.messages {
+	display: flex;
+	flex-direction: column;
+	align-items: flex-end;
+	margin: 20px 20px 0px 20px;
+	height: 90%;
+	overflow-y: auto;
+}
+
 /* Desktop styling */
-  .app {
-    display: grid; 
-    grid-template-columns: 5fr 12fr;
-    color: white; 
-  }
+.app {
+	display: grid;
+	grid-template-columns: 5fr 12fr;
+	color: white;
+}
 
-  .menu {
-    background-color: #191919;
-    background-image:
-      radial-gradient(circle at -15vw -40vh, #f2d54152, #191919 45%);
-    background-repeat: no-repeat;
-    height: 100vh;
-  }
+.menu {
+	background-color: #191919;
+	background-image:
+		radial-gradient(circle at -15vw -40vh, #f2d54152, #191919 45%);
+	background-repeat: no-repeat;
+	height: 100vh;
+}
 
-  .chat-window {
-    background-color: #101010;
-    background-image: 
-      radial-gradient(circle at top right, #4f22f223, #101010 30%),
-      radial-gradient(circle at bottom left, #4f22f213, #101010 50%), 
-      radial-gradient(circle at 50vw 30vh, #4f22f228, #101010 40%),
-      radial-gradient(circle at top left, #000000, #101010 40%);
-    background-size: 50% 50%;
-    background-position: top right, bottom left, bottom right, top left;
-    background-repeat: no-repeat;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-  }
+.chat-window {
+	background-color: #101010;
+	background-image:
+		radial-gradient(circle at top right, #4f22f223, #101010 30%),
+		radial-gradient(circle at bottom left, #4f22f213, #101010 50%),
+		radial-gradient(circle at 50vw 30vh, #4f22f228, #101010 40%),
+		radial-gradient(circle at top left, #000000, #101010 40%);
+	background-size: 50% 50%;
+	background-position: top right, bottom left, bottom right, top left;
+	background-repeat: no-repeat;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+}
+
 /* Mobile */
 </style>
---
+++ client/src/components/MeetButton.vue
@@ -0,0 +1,45 @@
+<script>
+import ForumOutlines from '../assets/svg/ForumOutlines.vue'; 
+
+export default { 
+    components: {
+        ForumOutlines
+    }
+};
+</script>
+<template>
+    <div class="meet">
+        <ForumOutlines id="forum-outlines" />
+        <h1>Meet</h1>
+    </div>
+</template>
+
+<style>
+
+.meet {
+    display: flex;
+    align-items: center;
+    justify-content: right;
+    margin: 0px 20px;
+    transition: transform 5s;
+}
+
+.meet h1 { 
+    font-size: 43px;
+    color: #6841F2;
+    margin: 0px;
+    padding: 0px;
+}
+
+.meet h1:hover { 
+    text-decoration: underline;
+    transform: scale(1.05);
+}
+
+#forum-outlines {
+    width: 50px;
+    height: 50px;
+    margin: 0px 10px;
+    fill: #6841F2;
+}
+</style>
