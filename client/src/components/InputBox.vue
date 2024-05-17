b05888971d909ecf2d1fd722fa101b100db3122c
WukoSiric <lstainer2002@gmail.com>
2024-05-17 16:38:11 +0930

    Created input box

---
+++ client/src/components/InputBox.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    props: {
+        modelValue: {
+            type: String,
+            required: true
+        },
+        placeholder: { 
+            type: String,
+            default: 'Enter text here',
+            required: false
+        }
+    },
+    data() {
+        return {
+            internvalValue: this.modelValue
+        }
+    },
+    watch: {
+            modelValue(newValue) {
+                this.internalValue = newValue;
+            },
+    },
+    methods: {
+        updateValue(event) {
+            this.internalValue = event.target.value;
+            this.$emit('update:modelValue', this.internalValue);
+        }
+    }
+};
+</script>
+
+<template>
+    <div>
+        <input id="input" type="text" autocomplete="off" :value="internalValue" @input="updateValue" :placeholder="placeholder"/>
+    </div>
+</template>
+
+<style>
+    #input { 
+        margin: 20px;
+        padding: 10px;
+        width: 90%;
+        border: 0px;
+        border-radius: 10px;
+    }
+
+    #input:focus {
+        outline: 1px solid rgb(88, 88, 88);
+    }
+</style>
