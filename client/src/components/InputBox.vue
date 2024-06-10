19463c4a227ff0799e2d3567eb6c1d44a34615d7
WukoSiric <lstainer2002@gmail.com>
2024-06-10 22:00:04 +0930

    Fixed typo

--- client/src/components/InputBox.vue
+++ client/src/components/InputBox.vue
@@ -1,57 +1,64 @@
 <script>
 export default {
-    props: {
-        modelValue: {
-            type: String,
-            required: true
-        },
-        placeholder: { 
-            type: String,
-            default: 'Enter text here',
-            required: false
-        }
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
     },
-    data() {
-        return {
-            internvalValue: this.modelValue
-        }
+    placeholder: {
+      type: String,
+      default: "Enter text here",
+      required: false,
     },
-    watch: {
-            modelValue(newValue) {
-                this.internalValue = newValue;
-            },
+  },
+  data() {
+    return {
+      internalValue: this.modelValue,
+    };
+  },
+  watch: {
+    modelValue(newValue) {
+      this.internalValue = newValue;
     },
-    methods: {
-        updateValue(event) {
-            this.internalValue = event.target.value;
-            this.$emit('update:modelValue', this.internalValue);
-        }
-    }
+  },
+  methods: {
+    updateValue(event) {
+      this.internalValue = event.target.value;
+      this.$emit("update:modelValue", this.internalValue);
+    },
+  },
 };
 </script>
 
 <template>
-    <div>
-        <input id="input" type="text" autocomplete="off" :value="internalValue" @input="updateValue" :placeholder="placeholder"/>
-    </div>
+  <div>
+    <input
+      id="input"
+      type="text"
+      autocomplete="off"
+      :value="internalValue"
+      @input="updateValue"
+      :placeholder="placeholder"
+    />
+  </div>
 </template>
 
 <style>
-    #input { 
-        margin: 20px;
-        padding: 10px;
-        width: 90%;
-        border: 0px;
-        border-radius: 10px;
-        font-size: 15px;
-        background-color: white;
-    }
+#input {
+  margin: 20px;
+  padding: 10px;
+  width: 90%;
+  border: 0px;
+  border-radius: 10px;
+  font-size: 15px;
+  background-color: white;
+}
 
-    #input::placeholder {
-        color: #464646;
-    }
+#input::placeholder {
+  color: #464646;
+}
 
-    #input:focus {
-        outline: 1px solid rgb(88, 88, 88);
-    }
+#input:focus {
+  outline: 1px solid rgb(88, 88, 88);
+}
 </style>
--- client/src/components/SearchBox.vue
+++ client/src/components/SearchBox.vue
@@ -1,79 +1,76 @@
 <script>
-import MagnifyIcon from '../assets/svg/MagnifyIcon.vue';
-
 export default {
-	components: {
-		MagnifyIcon
-	},
-	props: {
-		query: {
-			type: String,
-			required: true
-		},
-		placeholder: {
-			type: String,
-			default: 'Enter text here',
-			required: false
-		}		
-	},
-	data() {
-		return {
-			internvalValue: this.modelValue
-		}
-	},
-	watch: {
-		modelValue(newValue) {
-			this.internalValue = newValue;
-		},
-	},
-	methods: {
-		updateValue(event) {
-			this.internalValue = event.target.value;
-			this.$emit('update:modelValue', this.internalValue);
-		}
-	}
+  props: {
+    placeholder: {
+      type: String,
+      default: "Enter text here",
+      required: false,
+    },
+  },
+  data() {
+    return {
+      internalValue: this.modelValue,
+    };
+  },
+  watch: {
+    modelValue(newValue) {
+      this.internalValue = newValue;
+    },
+  },
+  methods: {
+    updateValue(event) {
+      this.internalValue = event.target.value;
+      this.$emit("update:modelValue", this.internalValue);
+    },
+  },
 };
 </script>
 
 <template>
-		<input id="search-box" type="text" autocomplete="off" :value="internalValue" @input="updateValue" :placeholder="placeholder" />
+  <input
+    id="search-box"
+    type="text"
+    autocomplete="off"
+    :value="internalValue"
+    @input="updateValue"
+    :placeholder="placeholder"
+  />
 </template>
 
 <style>
 ::placeholder {
-	color: rgb(197, 197, 197);
-	font-weight: 500;
+  color: rgb(197, 197, 197);
+  font-weight: 500;
 }
 
 #search-box {
-	margin: 20px;
-	padding: 10px;
-	width: 70%;
-	border: 3px solid white;
-	color: white;
-	border-radius: 10px;
-	font-size: 15px;
-	background-color: transparent;
-	font-size: 12pt;
-
+  margin: 20px;
+  padding: 10px;
+  width: 70%;
+  border: 3px solid white;
+  color: white;
+  border-radius: 10px;
+  font-size: 15px;
+  background-color: transparent;
+  font-size: 12pt;
 }
 
 #search-box:focus {
-	outline: 1px solid rgb(88, 88, 88);
+  outline: 1px solid rgb(88, 88, 88);
 }
 
 #search-box::placeholder {
-	color: rgb(192, 192, 192); 
+  color: rgb(192, 192, 192);
 }
 
 #search-box-container {
-	margin: 0;
-	padding: 0;
-	width: 0;
+  margin: 0;
+  padding: 0;
+  width: 0;
 }
 
 #search-icon {
-	fill: white;
-	max-height: 40px;
+  fill: white;
+  max-height: 40px;
 }
 </style>
